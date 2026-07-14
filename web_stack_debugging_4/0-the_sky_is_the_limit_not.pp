4a9d09a15e2adb5c12292eb67a2cc23b77877eb7
honore-lab <h.kamana@alustudent.com>
2026-07-14 09:37:16 +0200

    Fix Nginx worker connections and alignment for linting

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
-# Increase worker_connections to allow more concurrent requests
+# Increase Nginx worker_connections and restart service
 exec { 'increase_ulimit':
-  command => 'sed -i "s/worker_connections 768;/worker_connections 10240;/" /etc/nginx/nginx.conf',
+  command => '/bin/sed -i "s/worker_connections 768;/worker_connections 10240;/" /etc/nginx/nginx.conf',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
 exec { 'restart_nginx':
-  command => 'service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  subscribe => Exec['increase_ulimit'],
+  command     => '/usr/sbin/service nginx restart',
+  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  subscribe   => Exec['increase_ulimit'],
   refreshonly => true,
 }
