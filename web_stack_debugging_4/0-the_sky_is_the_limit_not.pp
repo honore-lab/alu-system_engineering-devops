e8790719ced6dc973138bbd833c69ad9d8e58147
honore-lab <h.kamana@alustudent.com>
2026-07-14 09:46:05 +0200

    Fix Nginx port and worker connections for high-concurrency performance

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,18 @@
-# Increase Nginx worker_connections and restart service
+# Increase Nginx worker_connections and ensure site is listening on port 80
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => file,
+  content => "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm;
+    server_name _;
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+}",
+}
+
 exec { 'increase_ulimit':
   command => '/bin/sed -i "s/worker_connections 768;/worker_connections 10240;/" /etc/nginx/nginx.conf',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
@@ -7,6 +21,6 @@ exec { 'increase_ulimit':
 exec { 'restart_nginx':
   command     => '/usr/sbin/service nginx restart',
   path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  subscribe   => Exec['increase_ulimit'],
+  subscribe   => [File['/etc/nginx/sites-enabled/default'], Exec['increase_ulimit']],
   refreshonly => true,
 }
